60bf5921ab7ad7843e08e5692c8b012030752206
Miolae <viktorprogger@gmail.com>
2018-08-21 14:51:50 +0300

    GMT-26 Test for AR accounts

--- tests/integration/ModuleCest.php
+++ tests/integration/ModuleCest.php
@@ -2,6 +2,7 @@
 
 namespace miolae\billing\tests\integration;
 
+use miolae\billing\models\Account;
 use miolae\billing\Module;
 
 class ModuleCest
@@ -14,6 +15,22 @@ class ModuleCest
 
         $accountFrom = 1;
         $accountTo = 2;
+        $amount = 3;
+        $invoice = $module->createInvoice($accountFrom, $accountTo, $amount);
+
+        $I->assertEquals($accountFrom, $invoice->account_id_from);
+        $I->assertEquals($accountTo, $invoice->account_id_to);
+        $I->assertEquals($amount, $invoice->amount);
+    }
+
+    public function createInvoiceWithAccountsAR(\IntegrationTester $I)
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        /** @var Module $module */
+        $module = \Yii::createObject(Module::class, ['billing']);
+
+        $accountFrom = Account::findOne(1);
+        $accountTo = Account::findOne(2);
         $amount = 3;
         $invoice = $module->createInvoice($accountFrom, $accountTo, $amount);
 
