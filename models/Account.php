db9f6eeaacfd584eab97dd534159eb86046a5614
miolae <viktorprogger@gmail.com>
2018-10-23 12:39:26 +0300

    GM-53 BlackHole validations

--- models/Account.php
+++ models/Account.php
@@ -49,12 +49,9 @@ class Account extends ActiveRecord
             [['type'], 'default', 'value' => static::TYPE_NORMAL],
             [['amount', 'hold', 'type', 'title'], 'required'],
             [['type', 'owner_id'], 'integer'],
-            [['amount', 'hold', 'owner_id',], 'number', 'min' => 0],
-            [['amount', 'hold'], function() {
-                if ($this->amountAvailable < 0) {
-                    $this->addError('Недостаточно средств на счете');
-                }
-            }],
+            [['hold', 'owner_id',], 'number', 'min' => 0],
+            [['amount', 'hold'], [$this, 'validateAmountAvailable']],
+            [['amount'], [$this, 'validateAmount']]
         ];
     }
 
@@ -71,5 +68,25 @@ class Account extends ActiveRecord
     public function getAmountAvailable()
     {
         return $this->amount - $this->hold;
+    }
+
+    public function validateAmount()
+    {
+        if (!is_numeric($this->amount)) {
+            $this->addError('Field \'amount\' must have a numeric value');
+
+            return;
+        }
+
+        if ($this->amount < 0 && $this->type !== static::TYPE_BLACKHOLE) {
+            $this->addError('Only BlackHole accounts can have negative amount');
+        }
+    }
+
+    public function validateAmountAvailable()
+    {
+        if ($this->type !== static::TYPE_BLACKHOLE || $this->amountAvailable < 0) {
+            $this->addError('Недостаточно средств на счете');
+        }
     }
 }
